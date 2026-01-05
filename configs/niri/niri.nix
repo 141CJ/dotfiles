dcb745f86539c0e01b356e7645c5c69b0fbf5986
141CJ <141cj+prod@proton.me>
2026-01-05 02:25:08 -0500

    delete niri overlay

--- configs/niri/niri.nix
+++ configs/niri/niri.nix
@@ -1,9 +1,5 @@
 { pkgs, inputs, ... }:
 {
-  imports = [ inputs.niri.homeModules.niri ];
-
-  nixpkgs.overlays = [ inputs.niri.overlays.niri ];
-
   home.file.".config/niri/config.kdl" = {
     source = ./config.kdl;
   };
